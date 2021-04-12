0555db65900766af34e9ae49a80547ee3e671a71
Safiyur Rahman <rahmansafiyur5850@gmail.com>
2021-04-12 12:12:27 +0100

    figured out the confusing storage api

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
 chrome.runtime.onInstalled.addListener(() => {
   console.log("RBLX Account Switcher | Ready");
+  chrome.storage.sync.set({ accounts: [] });
 });
--- script.js
+++ script.js
@@ -3,6 +3,16 @@ const mainOptions = document.getElementById("main-options");
 const switchAccountOptions = document.getElementById("switch-account-options");
 
 switchAccountButton.onclick = function () {
-  mainOptions.style.display = "none";
-  switchAccountOptions.style.display = "flex";
+  getAccounts(function (accounts) {
+    mainOptions.style.display = "none";
+    switchAccountOptions.style.display = "flex";
+    if (accounts.length == 0) switchAccountOptions.innerText = "No accounts!";
+  });
 };
+
+function getAccounts(callback) {
+  chrome.storage.sync.get(["accounts"], function (result) {
+    if (result.accounts.length == 0) callback([]);
+    callback(result.accounts);
+  });
+}
