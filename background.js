bc5bec111b8c9d1398fa182c33a17a6267cc95d3
Safiyur Rahman <rahmansafiyur5850@gmail.com>
2021-04-13 11:55:18 +0100

    figured out the bloody notification stuff

--- background.js
+++ background.js
@@ -11,11 +11,66 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
+chrome.runtime.onMessage.addListener(async (msg) => {
+  if (
+    msg.from == "switchAccountMenu" &&
+    msg.whatToDo == "setCookieAndCreateTab"
+  ) {
+    chrome.cookies.set({
+      url: "https://*.roblox.com/*",
+      httpOnly: true,
+      name: ".ROBLOSECURITY",
+      domain: ".roblox.com",
+    });
+    chrome.tabs.create(
+      {
+        url: "https://roblox.com",
+        selected: true,
+      },
+      function (tab) {
+        chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab2) => {
+          if (tabId == tab.id && changeInfo.status == "complete") {
+            handleSwitchAccountLoginStatus(tab2);
+          }
+        });
+      }
+    );
+  }
+});
+
+function handleSwitchAccountLoginStatus(tab2) {
+  if (tab2.url == "https://www.roblox.com/home") {
+    chrome.notifications.create(
+      {
+        message: "Seems to have logged in successfully!",
+        type: "basic",
+        title: "Login successful?",
+        iconUrl: "./A.png",
+      },
+      function (id) {
+        console.log(id, "logged in");
+      }
+    );
+  } else {
+    chrome.notifications.create(
+      {
+        message: "Login seems to have failed. The token probably expired.",
+        type: "basic",
+        title: "Login failed?",
+        iconUrl: "/A.png",
+      },
+      function (id) {
+        console.log(id, "not logged in");
+      }
+    );
+  }
+}
+
 function getCurrentUserInfo() {
   chrome.cookies.get(
     {
       name: ".ROBLOSECURITY",
-      url: "https://www.roblox.com/*",
+      url: "https://*.roblox.com/*",
     },
     function (cookie) {
       alert(JSON.stringify(cookie));
--- switchAccountMenu.js
+++ switchAccountMenu.js
@@ -2,7 +2,36 @@ export default function () {
   const userButtons = document.querySelectorAll("li.user-button");
   userButtons.forEach((button) => {
     button.onclick = function () {
-      alert(button.innerText);
+      chrome.runtime.sendMessage({
+        from: "switchAccountMenu",
+        whatToDo: "setCookieAndCreateTab",
+        username: button.innerText,
+      });
     };
   });
 }
+
+/*
+
+chrome.cookies.set(
+        {
+          url: "https://*.roblox.com/*",
+          httpOnly: true,
+          name: ".ROBLOSECURITY",
+          domain: ".roblox.com",
+        },
+        function (newCookie) {
+          chrome.tabs.create({
+            url: "https://roblox.com",
+          });
+          chrome.tabs.onCreated.addListener((tab) => {
+            console.error("hello world");
+            chrome.runtime.sendMessage({
+              greeting: "hi",
+              tab,
+            });
+          });
+        }
+      );
+
+ */
